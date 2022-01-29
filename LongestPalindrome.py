2b27fc9c0bd311bc990be19abfecc10d7b3f4cc6
awanesh92 <yawanesh@gmail.com>
2022-01-29 17:20:05 +0530

    Medium Program LeetCode Day36 Corrected LongestPalindrome.py file

--- LongestPalindrome.py
+++ LongestPalindrome.py
@@ -1,22 +1,22 @@
 class Solution:
-    def palindrome(self,s,i,j):
-        p=1
-        while i-p>0 and j+p<=len(s)-1:
-            if s[i-p]==s[p+j]:
-                p+1
+    def palindrome(self, s, i, j):
+        p = 1
+        while i - p >= 0 and j + p <= len(s) - 1:
+            if s[i - p] == s[j + p]:
+                p += 1
             else:
                 break
-        return s[i-p+1:j+p]
+        return s[i - p + 1:j + p]
 
     def longestPalindrome(self, s: str) -> str:
-        if len(s)==1:
+        if len(s) == 1:
             return s
-        p=''
+        p = ''
         for i in range(len(s)):
-            if s[i]==s[i-1]:
-                d=self.palindrome(s,i,i-1)
-                p=d if len(d)>len(p) else p
-            d=self.palindrome(s,i,i)
+            if s[i] == s[i - 1]:
+                d = self.palindrome(s, i - 1, i)
+                p = d if len(d) > len(p) else p
+            d = self.palindrome(s, i, i)
             p = d if len(d) > len(p) else p
         return p
 
