63947673e4fd547c956afca4b747a63cc9496d7e
awanesh92 <yawanesh@gmail.com>
2022-03-27 14:36:51 +0530

    Medium Program LeetCode Day45 Insert Interval solution

---
+++ NewInterval.py
@@ -0,0 +1,18 @@
+from typing import List
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        if len(intervals)==0:
+            return [newInterval]
+
+        fininterval=sorted(intervals+[newInterval])
+        out=[fininterval[0]]
+        for i in range(1,len(fininterval)):
+            if fininterval[i][0]<=out[-1][1]:
+                out[-1][1]=max(fininterval[i][1],out[-1][1])
+            else:
+                out.append(fininterval[i])
+        return out
+
+s=Solution()
+assert  [[1,5],[6,9]]==s.insert([[1,3],[6,9]],[2,5])
+assert [[1,2],[3,10],[12,16]]==s.insert([[1,2],[3,5],[6,7],[8,10],[12,16]],[4,8])
